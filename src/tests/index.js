04cd0c56afb75104b5ed5060a93d353abcdf6ed4
Vera Protopopova <vera.protopopova@gmail.com>
2023-02-23 19:30:03 -0500

    save changes

--- src/tests/index.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-import Playa from './../dist';
-
-const playa = Playa({});
-
-playa.v1.asset.upload([{filePath: "./path-to-file"}])
---
+++ src/tests/index.ts
@@ -0,0 +1,7 @@
+'use strict';
+
+import Playa from "../index"
+
+const playa = Playa({});
+
+playa.v1.asset.upload([{filePath: "suckkckc"}])

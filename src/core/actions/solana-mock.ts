3c0a43877ea528a90480ac91cfc4d6ad18651942
mark zuppe <markzuppe@macbook-pro.myfiosgateway.com>
2023-02-25 21:14:42 -0500

    solana inputs and mock returns

---
+++ src/core/actions/solana-mock.ts
@@ -0,0 +1,26 @@
+import { Keypair, PublicKey } from "@solana/web3.js";
+import { SolanaAssetCreateInput, SolanaTaxonomyCreateInput } from "../types/solana";
+
+const generateKey = () => new Keypair().publicKey.toString();
+const simulateProcessing = () => new Promise((resolve) => setTimeout(resolve, 5000));
+
+const taxonomyCreateAccounts = async (params: [SolanaTaxonomyCreateInput]) => {
+    await simulateProcessing();
+    return params.map((param) => ({publicKey: generateKey(), label: param.label, parent: param.parent, level: 0, updated: new Date().toISOString()}));
+}
+
+const assetCreateAccounts = async (params: SolanaAssetCreateInput[]) => {
+    await simulateProcessing();
+    return params.map((param) => ({publicKey: generateKey(), arweaveId: param.arweaveId, updated: new Date().toISOString()}));
+}
+
+const templateCreateAccounts = async (params: SolanaAssetCreateInput[]) => {
+    await simulateProcessing();
+    const owner = generateKey()
+    return params.map((param) => ({publicKey: owner, arweaveId: param.arweaveId, owner, private: false,  updated: new Date().toISOString()}));
+}
+
+const entryCreateAccounts = async (params: SolanaAssetCreateInput[]) => {
+    const owner = generateKey()
+    return params.map((param) => ({publicKey: owner, arweaveId: param.arweaveId, owner, private: param.private ?? false, immutable: param.immutable ?? false, taxonomy: param.taxonomy ?? [], updated: new Date().toISOString()}));
+}
---
+++ src/types/solana.ts
@@ -0,0 +1,34 @@
+import { PublicKey, Keypair } from "@solana/web3.js";
+
+type SolanaTaxonomyCreateInput = {
+    label: string;
+    payer: Keypair;
+    owner?: PublicKey 
+    parent?: PublicKey;
+};
+
+type SolanaAssetCreateInput = {
+    arweaveId: string;
+    payer: Keypair;
+    owner?: PublicKey 
+}
+
+type SolanaTemplateCreateInput = {
+    arweaveId: string;
+    payer: Keypair;
+    owner?: PublicKey 
+    private?: boolean;
+}
+
+type SolanaEntryCreateInput = {
+    arweaveId: string;
+    payer: Keypair;
+    owner?: PublicKey 
+    private?: boolean;
+    immutable?: boolean;
+    taxonomy?: PublicKey[]
+}
+
+
+
+export type { SolanaTaxonomyCreateInput, SolanaAssetCreateInput, SolanaTemplateCreateInput, SolanaEntryCreateInput};

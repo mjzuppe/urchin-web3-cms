f993a96ba309153d98475e23bb8c296e3aaf0936
Vincent <vincent.veysset.canada@gmail.com>
2023-03-13 20:32:57 +0100

    Add pricing

--- src/core/actions/global.ts
+++ src/core/actions/global.ts
@@ -6,20 +6,52 @@ import { getTemplatesQueues, processTemplates } from './template';
 import { processEntries } from './entry';
 import { processAssets } from './asset';
 
+const PRICING: any = {
+  asset: { create: 3002800, update: 5000 },
+  entry: { create: 3928480, update: 5000 },
+  taxonomy: { create: 2098000, update: 5000 },
+  template: { create: 3225520, update: 5000 },
+}
 
-const processAll = async (props:any) => {
-  return {
+const _calculatePricing = (payload: any) => {
+  const cost = { lamports: 0, sol: 0 };
+  const features = ['asset', 'entry', 'taxonomy', 'template'];
+
+  for (const feature of features) {
+    cost.lamports += payload[feature].create.length * PRICING[feature].create;
+    cost.lamports += payload[feature].update.length * PRICING[feature].update;
+  }
+
+  cost.sol = cost.lamports * 0.000000001;
+
+  return cost;
+};
+
+const processAll = async (props: any) => {
+  const features: any = {
+    asset: getAssetsQueues(),
+    entry: getEntriesQueues(),
+    taxonomy: getTaxonomiesQueues(),
+    template: getTemplatesQueues(),
+  };
+
+  const payload: any = {
+    asset: await processAssets(props),
     completed: true, //TODO MZ: inner logic if fails
+    entry: await processEntries(props),
     taxonomy: await processTaxonomies(props),
     template: await processTemplates(props),
-    entry: await processEntries(props),
-    asset: await processAssets(props),
   };
+
+  payload.cost = _calculatePricing(features);
+
+  return payload;
 };
 
 const queryAll = async (props:any) => {
   const { cluster, payer, rpc } = await loadSolanaConfig(props);
-  return {
+
+  const payload: any = {
     asset: getAssetsQueues(),
     cluster,
     entry: getEntriesQueues(),
@@ -28,6 +60,10 @@ const queryAll = async (props:any) => {
     taxonomy: getTaxonomiesQueues(),
     template: getTemplatesQueues(),
   };
+
+  payload.cost = _calculatePricing(payload);
+
+  return payload;
 };
 
 export { processAll, queryAll };

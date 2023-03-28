9b7416e2d7a37412d133495707c97b89c261cd1c
mark zuppe <markzuppe@marks-mbp.myfiosgateway.com>
2023-03-27 21:37:41 -0400

    update bundlr to node2 - free

--- src/services/arweave/metadata/upload-data.ts
+++ src/services/arweave/metadata/upload-data.ts
@@ -18,13 +18,13 @@ import { WebBundlr } from "@bundlr-network/client";
 
 const uploadData = async (secret: Keypair, cluster: Cluster, data: any, wallet: any = null) => {
     if (wallet) {
-        const bundlr = new WebBundlr("https://devnet.bundlr.network", "solana", wallet, {providerUrl: "https://api.devnet.solana.com"}); // TODO dynamic for mainnet
-        await bundlr.fund(100000);
+        const bundlr = new WebBundlr("https://node2.bundlr.network", "solana", wallet, {providerUrl: "https://api.devnet.solana.com"}); // TODO dynamic for mainnet ---previously https://devnet.bundlr.network
+        // await bundlr.fund(100000);
         return await bundlr.upload(JSON.stringify(data));
     }
     else {
-        const bundlr = new Bundlr("https://devnet.bundlr.network", "solana", secret, {providerUrl: "https://api.devnet.solana.com"}); // TODO dynamic for mainnet
-        await bundlr.fund(100000);
+        const bundlr = new Bundlr("https://node2.bundlr.network", "solana", secret, {providerUrl: "https://api.devnet.solana.com"}); // TODO dynamic for mainnet
+        // await bundlr.fund(100000);
         return await bundlr.upload(JSON.stringify(data));
     }
 
